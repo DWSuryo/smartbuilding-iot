fde9aa3cee5074aae71b4171fe96c47ef4c45c25
DWLight <ditowicaksono2607@gmail.com>
2021-05-14 17:29:17 +0700

    may 14 2021

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,13 +1,16 @@
 import os
 import secrets
+import configparser
 
 class Config:
+    conf = configparser.ConfigParser()
+    conf.read("credential.ini")
     #import secrets --> secrets.token_hex(16)
     
     #SECRET_KEY = secrets.token_hex(16)
     #SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = '5da34e07ea6f3c2c10aaba508b262c95'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = conf["Flask"]["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = conf["Flask"]["SQLALCHEMY_DATABASE_URI"]
     #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
